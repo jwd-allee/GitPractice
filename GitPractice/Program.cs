0421533e3e1a78f8a5d3565ec77ad9cb4ae7de40
Lee, Alexander <AlLee@quantaservices.com>
2020-10-14 11:26:50 -0400

    Made another change.

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -6,7 +6,7 @@ namespace GitPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Change.");
+            Console.WriteLine("Hello World! Change. Another change.");
         }
     }
 }
